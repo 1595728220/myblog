a307e011ee1d90ffb5348296800b2ca7124873a4
苏琨博 <1595728220@qq.com>
2019-12-06 16:04:19 +0800

    定制样式，引入基本的样式文件，全局方法js文件改写为ts

---
+++ src/global/baseMethod.ts
@@ -0,0 +1,217 @@
+export default {
+  install(Vue: any, options: any) {
+    // 验证数据是否为空
+    Vue.prototype.$isEmpty = (value: any) => {
+      // 判断字符串是否为空
+      switch (typeof value) {
+        case "undefined":
+          return true;
+        case "boolean":
+          return false;
+        case "string":
+          return value === "";
+        case "number":
+          return isNaN(value);
+        case "object":
+          // 数组
+          if (Array.isArray(value)) {
+            return value.length === 0;
+          } else {
+            // null
+            if (value === null) {
+              return true;
+            }
+            // 其他对象统一不认为为空
+            if (Object.prototype.toString.call(value) !== "[object Object]") {
+              return false;
+            }
+            // 普通对象如果可枚举属性为空认为该对象为空
+            return Object.keys(value).length === 0;
+          }
+        default:
+          return true;
+      }
+    };
+    // 转换字符
+    Vue.prototype.$replaceAllStr = (
+      value: string,
+      regexp: any,
+      substr: string
+    ) => {
+      if (Vue.$isEmpty(value)) {
+        return "";
+      }
+      return value.replace(new RegExp(regexp, "gm"), substr);
+    };
+    // 根据value从List中获取对应label值
+    Vue.prototype.$getListValue = (
+      value: any,
+      list: Array<{ label: ""; value: "" }>
+    ) => {
+      // 对象数组的类型不是数组
+      if (!Array.isArray(list)) {
+        return "";
+      }
+      let result:
+        | { label: string | number; value: string | number }
+        | undefined = list.find((el: any) => value === el.value);
+      // 如果找不到对应对象获取对象的label属性为空
+      if (!result || !result.label) {
+        return "";
+      }
+      return result.label;
+    };
+    Vue.prototype.$regSpecial = (str: any) => {
+      //
+      if (typeof str !== "string") {
+        return { value: false, msg: "请传入一个字符串类型数据" };
+      }
+      if (str === "") {
+        return { value: false, msg: "请传入非空数据" };
+      }
+      let regEn = /[`~!@#$%^&*()_+<>?:"{},./;'[\]]/im,
+        regCn = /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im;
+      if (regEn.test(str) || regCn.test(str)) {
+        return { value: false, msg: "包含特殊字符" };
+      } else {
+        return { value: true, msg: "验证成功" };
+      }
+    };
+    // 数组去重
+    Vue.prototype.$removal = (arr: any) => {
+      if (!Array.isArray(arr)) {
+        return arr;
+      }
+      return Array.from(new Set(arr));
+    };
+    // 根据当前时间推算本月总天数
+    Vue.prototype.$monthCalcDay = (time: any) => {
+      time = new Date(time);
+      let year = time.getFullYear(),
+        // 获取当前月的下个月份
+        month = time.getMonth() + 2;
+      // 当前月为12月时，算出下个月的年份和月份
+      if (month === 13) {
+        month = 1;
+        year++;
+      }
+      let tmpArr: string = [year, month, "1"].join("/");
+      let endDayTime = new Date(tmpArr).getTime() - 86400000;
+      return new Date(endDayTime).getDate();
+    };
+    // 比较日期相等
+    Vue.prototype.$dateEqual = (time1: any, time2: any) => {
+      time1 = new Date(time1);
+      time2 = new Date(time2);
+      if (time1.getFullYear() !== time2.getFullYear()) {
+        return false;
+      }
+      if (time1.getMonth() !== time2.getMonth()) {
+        return false;
+      }
+      if (time1.getDate() !== time2.getDate()) {
+        return false;
+      }
+      return true;
+    };
+    /**
+     * time 时间对象
+     * tag 传入想要得到的时间格式 yyyy-MM-dd HH-mm-ss
+     */
+    Vue.prototype.$tagTime = (time: any, tag: any) => {
+      if (!tag) {
+        return "";
+      }
+      if (!time) {
+        return "";
+      }
+      let tmpTime: any = new Date(time);
+      if (isNaN(tmpTime)) {
+        return "";
+      }
+      let year = tmpTime.getFullYear(),
+        month = tmpTime.getMonth() + 1,
+        date = tmpTime.getDate(),
+        hour = tmpTime.getHours(),
+        minute = tmpTime.getMinutes(),
+        second = tmpTime.getSeconds();
+      // 不足10前面补0
+      if (month < 10) {
+        month = "0" + month;
+      }
+      // 不足10前面补0
+      if (date < 10) {
+        date = "0" + date;
+      }
+      // 不足10前面补0
+      if (hour < 10) {
+        hour = "0" + hour;
+      }
+      // 不足10前面补0
+      if (minute < 10) {
+        minute = "0" + minute;
+      }
+      // 不足10前面补0
+      if (second < 10) {
+        second = "0" + second;
+      }
+      return tag
+        .replace("yyyy", year)
+        .replace("MM", month)
+        .replace("dd", date)
+        .replace("HH", hour)
+        .replace("mm", minute)
+        .replace("ss", second);
+    };
+    Vue.prototype.$debounce = (() => {
+      let lastTime: any = new Date();
+      return (callback: any, wait: any) => {
+        let currentTime: any = new Date();
+        let remaining: any = currentTime - lastTime - wait;
+        if (remaining >= 0) {
+          callback();
+          lastTime = currentTime;
+        }
+      };
+    })();
+    /**
+     * 将下标为index1的元素插入到index2中返回数组
+     */
+    Vue.prototype.$displace = (index1: any, index2: any, array: any) => {
+      let newArray = [...array],
+        tmpVal = newArray[index1],
+        max,
+        min,
+        oneArray,
+        twoArray,
+        threeArray;
+      if (index1 > index2) {
+        min = index2;
+        max = index1;
+        oneArray = newArray.slice(0, min);
+        twoArray = newArray.slice(min, max);
+        threeArray = newArray.slice(max + 1, newArray.length);
+        return [...oneArray, tmpVal, ...twoArray, ...threeArray];
+      } else {
+        min = index1;
+        max = index2;
+        oneArray = newArray.slice(0, min);
+        twoArray = newArray.slice(min + 1, max + 1);
+        threeArray = newArray.slice(max + 1, newArray.length + 1);
+        return [...oneArray, ...twoArray, tmpVal, ...threeArray];
+      }
+    };
+    // 从目标数组中删除给定下标的元素
+    Vue.prototype.$deleteIndexOfArray = (index: any, list: any) => {
+      if (!Array.isArray(list)) {
+        return;
+      }
+      return [...list.slice(0, index), ...list.slice(index + 1, list.length)];
+    };
+    // 对传入的参数按指定的标签进行拼接
+    Vue.prototype.$splicing = (tag: any, ...arr: any) => {
+      arr = arr.filter((el: any) => el);
+      return arr.join(tag);
+    };
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,32 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ViewUI from "view-design";
+
+// 公共方法
+import baseMethod from "./global/baseMethod";
+// import "view-design/dist/styles/iview.css";
+import "../my-theme/dist/iview.css";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $isEmpty: any;
+    $replaceAllStr: any;
+    $getListValue: any;
+    $regSpecial: any;
+    $removal: any;
+    $monthCalcDay: any;
+    $dateEqual: any;
+    $tagTime: any;
+    $debounce: any;
+    $displace: any;
+    $deleteIndexOfArray: any;
+    $splicing: any;
+  }
+}
 
 Vue.use(ViewUI);
+Vue.use(baseMethod);
 Vue.config.productionTip = false;
 
 new Vue({
